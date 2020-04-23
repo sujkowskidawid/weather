a9bb277a8088b21260384088aa6a43d589dda7f7
sujkowskidawid <sujkowskidawid942gmail.com>
2020-04-23 15:51:58 +0200

    end

---
+++ src/main/java/Weather.java
@@ -0,0 +1,37 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.text.DecimalFormat;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(includeFieldNames = false)
+
+public class Weather {
+
+    private Double temperature;
+
+
+    private Double clouds;
+
+
+    private Double pressure;
+
+    private Double humidity;
+    private String data;
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("#.##");
+        return "\nData = '" + data +
+                ", Temperatura=" + df.format(temperature) + " \u00b0C"+
+                ", Zachmurzenie =" + clouds + " %" +
+                ", Ciśnienie =" + pressure + " hPa "+
+                ", Wilgotność =" + humidity +" % "+
+                '\'';
+
+    }
+}
